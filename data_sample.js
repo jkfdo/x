e90a8512529494f2f92af82e55e961799ddf9be5
jkfdo <obidev@proton.me>
2025-10-31 19:25:29 +0600

    Create data_sample.js

---
+++ data_sample.js
@@ -0,0 +1,93 @@
+const data = [ 
+  {
+    name: ``,
+    artist: ``,
+    subArtist: ``,
+    genre: ``,
+    mood: ``,
+    
+    channel: `AurVen`,
+    publishDate: ``,
+    pubLink = ``,
+    
+    srcLink = ``,
+    srcVolume = ``,
+    
+    artwork: ``,
+    art: ``,
+    artf: ``,
+  },
+  
+   {
+     name: ``,
+     artist: ``,
+     subArtist: ``,
+     genre: ``,
+     mood: ``,
+     
+     channel: `AurVen`,
+     publishDate: ``,
+     pubLink = ``,
+     
+     srcLink = ``,
+     srcVolume = ``,
+     
+     artwork: ``,
+     art: ``,
+     artf: ``,
+   },
+   {
+     name: ``,
+     artist: ``,
+     subArtist: ``,
+     genre: ``,
+     mood: ``,
+     
+     channel: `AurVen`,
+     publishDate: ``,
+     pubLink = ``,
+     
+     srcLink = ``,
+     srcVolume = ``,
+     
+     artwork: ``,
+     art: ``,
+     artf: ``,
+   },
+   {
+     name: ``,
+     artist: ``,
+     subArtist: ``,
+     genre: ``,
+     mood: ``,
+     
+     channel: `AurVen`,
+     publishDate: ``,
+     pubLink = ``,
+     
+     srcLink = ``,
+     srcVolume = ``,
+     
+     artwork: ``,
+     art: ``,
+     artf: ``,
+   },
+   {
+     name: ``,
+     artist: ``,
+     subArtist: ``,
+     genre: ``,
+     mood: ``,
+     
+     channel: `AurVen`,
+     publishDate: ``,
+     pubLink = ``,
+     
+     srcLink = ``,
+     srcVolume = ``,
+     
+     artwork: ``,
+     art: ``,
+     artf: ``,
+   },
+]
